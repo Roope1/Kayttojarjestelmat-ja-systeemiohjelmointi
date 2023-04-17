4ab3972f2bc2bce808da9167a57c490051f1b07d
Roope1 <roopeh.turkki@gmail.com>
2023-04-17 19:35:28 +0300

    zip and unzip code done

--- Project2/my-unzip.c
+++ Project2/my-unzip.c
@@ -1,5 +1,48 @@
-int main(void)
+#include <stdio.h>
+#include <stdlib.h>
+
+void readfile(char *filename);
+
+int main(int argc, char *argv[])
 {
 
+    /* Not enough arguments */
+    if (argc < 2)
+    {
+        fprintf(stderr, "my-unzip: file1 [file2...]\n");
+        exit(1);
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        readfile(argv[i]);
+    }
+
     return 0;
+}
+
+void readfile(char *filename)
+{
+    FILE *fp;
+    char current_char = '1'; // initialized with a number to know for first time check
+    int count = 0;
+
+    fp = fopen(filename, "rb");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "my-unzip: cannot open file\n");
+        exit(1);
+    }
+
+    // get first 4 bytes for number of characters
+    while (fread(&count, sizeof(int), 1, fp))
+    {
+        fread(&current_char, sizeof(char), 1, fp);
+        // print current_char count amount of times
+        for (int i = 0; i < count; i++)
+        {
+            fprintf(stdout, "%c", current_char);
+        }
+    }
+    fclose(fp);
 }
--- Project2/my-zip.c
+++ Project2/my-zip.c
@@ -1,5 +1,79 @@
-int main(void)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 512
+
+void readfile(char *filename);
+
+int main(int argc, char *argv[])
 {
 
+    /* Not enough arguments */
+    if (argc < 2)
+    {
+        fprintf(stderr, "my-zip: file1 [file2...]\n");
+        exit(1);
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        readfile(argv[i]);
+    }
+
     return 0;
+}
+
+void readfile(char *filename)
+{
+    FILE *fp;
+    char buffer[BUFFER_SIZE];
+    char current_char = '1'; // initialized with a number to know for first time check
+    int count = 0;
+
+    fp = fopen(filename, "r");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "my-zip: cannot open file\n");
+        exit(1);
+    }
+
+    while (fgets(buffer, BUFFER_SIZE, fp) != NULL)
+    {
+        for (int i = 0; i < BUFFER_SIZE - 1; i++)
+        {
+            if (buffer[i] == '\n') // end of line continue to next
+            {
+                continue;
+            }
+
+            if (buffer[i] == '\0') // end of doc
+            {
+                fwrite(&count, sizeof(int), 1, stdout);
+                fwrite(&current_char, sizeof(char), 1, stdout);
+                break;
+            }
+
+            // initialize current_char with the first character of the buffer
+            // FOR 1st PASSTHROUGH ONLY
+            if (current_char == '1')
+            {
+                current_char = buffer[0];
+            }
+
+            /* character has switched == write down results*/
+            if (buffer[i] != current_char)
+            {
+                fwrite(&count, sizeof(int), 1, stdout);
+                fwrite(&current_char, sizeof(char), 1, stdout);
+                current_char = buffer[i];
+                count = 1;
+            }
+            else
+            {
+                count++;
+            }
+        }
+    }
+    fclose(fp);
 }
